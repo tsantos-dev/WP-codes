a54ad345529d8b32ace3c895079b0b6fea9bf7b9
Thiago Santos <134621239+tsantos-dev@users.noreply.github.com>
2023-07-26 08:35:15 -0400

    Create remove-query-arg.php

---
+++ remove-query-arg.php
@@ -0,0 +1,10 @@
+<?php 
+// Remove query string from static files
+// Insert in functions.php
+function remove_cssjs_ver( $src) {
+	if( strpos( $src, 'ver=' ) )
+	$src = remove_query_arg( 'ver', $src );
+return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );

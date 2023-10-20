a7936522127ebb8d49f659e256e96f550999e583
Thiago Santos <134621239+tsantos-dev@users.noreply.github.com>
2023-10-20 15:32:03 -0400

    Update metabox-insert-pdf.php
    Inserindo função para gerar shortcode para embbedar o arquivo no the_content e correção de variável $link

--- metabox-insert-pdf.php
+++ metabox-insert-pdf.php
@@ -8,8 +8,7 @@ function pdf_init(){
 function pdf_link(){
     global $post;
     $custom  = get_post_custom($post->ID);
-    // $link    = $custom["link"][0];
-    $link    = [];
+    $link    = $custom["link"][0];
     $count   = 0;
     echo '<p>Selecione a lei que está relacionado ao item</p>';
     echo '<div class="link_header">';
@@ -35,6 +34,7 @@ function pdf_link(){
     }
     echo '</select><br /></div>';
     echo '<div class="pdf_count"><span>PDF\'s encontrados:</span> <b>'.$count.'</b></div>';
+    echo '<div class="pdf_shortcode"><span>Shortcode para inserir no texto: </span><b>[pdf_embbed]</b></div>';
 }
 function save_pdf_link(){
     global $post;
@@ -61,6 +61,16 @@ function pdf_css() {
         border-radius:0px 0px 6px 6px;
         }
     .pdf_count span{color:#666;}
+    .pdf_shortcode{
+		font-size:9px;
+        color:#0066ff;
+		background:#f3f3f3;
+        border-top:solid 1px #e5e5e5;
+        padding:6px 6px 6px 12px;
+        margin:0px -6px -8px -6px;
+        -moz-border-radius:0px 0px 6px 6px;
+        -webkit-border-radius:0px 0px 6px 6px;
+        border-radius:0px 0px 6px 6px;}
         </style>';
 }
 function pdf_file_url(){
@@ -70,6 +80,19 @@ function pdf_file_url(){
         echo $custom['link'][0];
     }
 }
+
+//shortcode para embbedar o arquivo no the_content
+function pdf_shortcode($atts, $content = null) {
+	$pdf = get_post_meta( get_the_ID(), 'link' );
+	extract(shortcode_atts(array(
+		"id" => 'pdf_embbeded',
+		"url" => $pdf[0],
+		"width" => '100%',
+		"height" => '743px'
+	), $atts));
+	return '<iframe id="' . $id . '" src="http://docs.google.com/viewer?url=' . $url . '&embedded=true" style="width:' . $width . '; height:' . $height . ';" frameborder="0"></iframe>';
+}
+add_shortcode('pdf_embbed', 'pdf_shortcode');
 ?>
 
 <!-- inserir no local de exibição do link para o PDF -->
